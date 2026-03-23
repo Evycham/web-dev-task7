cd274201c71ddeb4240e30b7d0a451957320d0ba
evycham <ygruzevich8484@gmail.com>
2026-03-23 23:57:37 +0100

    .

--- app.mjs
+++ app.mjs
@@ -1,29 +1,42 @@
 'use strict';
 
 import express from 'express';
+import crypto from 'crypto';
 
 const app = express();
 const port = 3000;
 
 const tasks = []
 
-app.post('/', (req, res) => {
-    let title;
-    let description;
-    let date;
+app.use(express.json());
 
-    try{
-        title = req.body.title;
-        description = req.body.description;
-        date = req.body.date;
+app.post('/todos', (req, res) => {
 
-        const task = new Task(title, description, date);
-        tasks.push(task);
-    } catch(err){
-        res.status(400);
+    const { title, description, date } = req.body;
+
+    if(typeof title !== 'string' || title.trim() === '') {
+        return res.status(400).json({error: 'Missing title'});
     }
-    res.status(201);
+
+    if(typeof description !== 'string' && description.trim() === '') {
+        return res.status(400).json({error: 'Missing description'});
+    }
+
+    if(typeof date !== 'string' || date.trim() === '') {
+        return res.status(400).json({error: 'Missing date'});
+    }
+
+    const task = new Task(title, description, date);
+    tasks.push(task);
+
+    return res.status(201).json(task);
 });
+
+app.get('/todos', (req, res) => {
+    res.status(200).json(tasks);
+});
+
+
 
 app.listen(port, () => {
     console.log('listening on port:', port);
